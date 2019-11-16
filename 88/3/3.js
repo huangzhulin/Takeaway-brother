4c4f21796ddc0934f550e5cd3159f9836bd2c3b0
huangzhuling <44010218+huangzhuling@users.noreply.github.com>
2019-11-16 08:08:39 +0800

    更新

---
+++ 88/3/3.js
@@ -0,0 +1,14 @@
+function big(x){
+    document.getElementsByClassName("texets")[0].style.display="inline";
+}
+function normal(x){
+    document.getElementsByClassName("texets")[0].style.display="none";
+}
+
+function big1(x){
+    document.getElementsByClassName("texets1")[0].style.display="inline";
+}
+function normal1(x){
+    document.getElementsByClassName("texets1")[0].style.display="none";
+}
+console.log();
